666999fd9d000799854cd0597a18384bedb9ffff
谭源 <kentertan12138@Outlook.com>
2021-03-28 23:58:22 +0800

    Replace deprecated method

--- src/test/scala/ALU/testALU.scala
+++ src/test/scala/ALU/testALU.scala
@@ -1,7 +1,13 @@
 package ALU
 
-import chisel3.Driver
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import firrtl.options.TargetDirAnnotation
 
+//test:runMain ALU.testALU
 object testALU extends App {
-  Driver.execute(args, () => new ALU)
+  (new chisel3.stage.ChiselStage).execute(
+    Array("-X", "verilog", "--full-stacktrace"),
+    Seq(ChiselGeneratorAnnotation(() => new ALU()),
+      TargetDirAnnotation("generated/ALU"))
+  )
 }
--- src/test/scala/test_basic/testMain.scala
+++ src/test/scala/test_basic/testMain.scala
@@ -1,7 +1,13 @@
 package test_basic
 
-import chisel3.Driver
+import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
+import firrtl.options.TargetDirAnnotation
 
+//test:runMain test_basic.testMain
 object testMain extends App {
-  Driver.execute(args, () => new AND)
+  (new chisel3.stage.ChiselStage).execute(
+    Array("-X", "verilog", "--full-stacktrace"),
+    Seq(ChiselGeneratorAnnotation(() => new AND()),
+      TargetDirAnnotation("generated/and"))
+  )
 }
