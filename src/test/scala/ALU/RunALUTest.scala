8f8fc6068f057d9665d4df91071947cfff7d92c0
谭源 <kentertan12138@outlook.com>
2021-04-12 18:02:33 +0800

    Generate VCD file Reference: https://github.com/chipsalliance/chisel3/issues/889

--- src/test/scala/ALU/RunALUTest.scala
+++ src/test/scala/ALU/RunALUTest.scala
@@ -1,6 +1,6 @@
 package ALU
 
-//test:runMain ALU.RunALUTest -td ./generated/ALUTest --is-verbose
+//test:runMain ALU.RunALUTest -td ./generated/ALUTest --is-verbose --generate-vcd-output on
 object RunALUTest extends App {
   chisel3.iotesters.Driver.execute(args, () => new ALU)(c => new ALUTest(c))
 }
